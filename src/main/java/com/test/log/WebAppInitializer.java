bd22a6ff255541e6ba35bee17c5b1aed5539694b
Dmitry Sey <d.sey@itransition.com>
2019-07-28 23:58:39 +0300

    #API  * add web context initializer  * add gradle wrapper  * add hibernate configuration

---
+++ src/main/java/com/test/log/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package com.test.log;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.test.log")
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/*"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{WebAppInitializer.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+}
---
+++ src/main/java/com/test/log/config/AppConfig.java
@@ -0,0 +1,56 @@
+package com.test.log.config;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.logging.Log;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableAspectJAutoProxy
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+    @Value("${db.url}")
+    private String dbUrl;
+    @Value("${db.username}")
+    private String dbName;
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Bean
+    public SessionFactory sessionFactory(DataSource dataSource) {
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        settings.put(Environment.DATASOURCE, dataSource);
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "org.hibernate.context.internal.ThreadLocalSessionContext");
+
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(Log.class);
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build();
+        return configuration.buildSessionFactory(registry);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        basicDataSource.setUsername(dbName);
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setPassword(dbPassword);
+        return basicDataSource;
+    }
+}
